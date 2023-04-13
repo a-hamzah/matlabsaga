0893014ac072adff6984027ddbce486c030da0c0
Ameer Hamzah <ameer74860@outlook.com>
2023-04-13 05:28:26 +0500

    small mistake corrected

--- NUST-RMC-Spring23/Slides_Tasks/Lec23_4.m
+++ NUST-RMC-Spring23/Slides_Tasks/Lec23_4.m
@@ -11,6 +11,7 @@ R = eul2r(0.1, 0.2, 0.3)
 gamma = tr2eul(R) 
 
 %problem if theta is negative, different angles are returned but same rotation matrix
-R = eul2r(0.1 , -0.2, 0.3) tr2eul(R)  
+R = eul2r(0.1 , -0.2, 0.3)
+tr2eul(R)  
 % problematic since mapping from a rotation matrix to Euler angles is not unique and the Toolbox always returns a positive angle for θ 
 eul2r(ans)
