e923458a550b84fd96c895c250c64b3a4daebc10
a-hamzah <Spellbreaker907@gmail.com>
2023-09-02 19:29:11 +0500

    legged

---
+++ Legged_Robotics/test_file.m
@@ -0,0 +1,8 @@
+clc
+clear
+close all
+
+t = linspace(0,2*pi,100);
+y = sin(t);
+
+plot(t,y, 'b*:')
--- NUST-RMC-Spring23/Slides_Tasks/Lec45_5.m
+++ NUST-RMC-Spring23/Slides_Tasks/Lec45_5.m
@@ -6,16 +6,16 @@ close all
 
 % Defining DH Parameters for planar 2-join robot (Slide 96)
 
-dh_parameters = [0 0 1 0; 0 0 1 0];  % a1,a2 = 1 meter
+dh_table = [0 0 1 0; 0 0 1 0];  % a1,a2 = 1 meter
 
 % Creating a robot
 
-Robot = SerialLink(dh_parameters)
+Robot = SerialLink(dh_table)
 
 % Robot.tool
 % Robot.gravity
 % Robot.base
 
-Robot.fkine([0.3 0.9])
-Robot.plot([0.3 0.9])
+Robot.fkine([0.2 0.3])
+Robot.plot([0.2 0.3])
 Robot.teach
